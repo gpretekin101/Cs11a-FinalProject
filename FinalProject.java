979f4da02ef67fa4c9ae49a89b51b5170b93826b
gpretekin101 <33820480+gpretekin101@users.noreply.github.com>
2017-11-30 17:08:21 -0500

    Add files via upload

---
+++ FinalProject.java
@@ -0,0 +1,12 @@
+public class FinalProject {
+  public static void main(String[] args){
+    boolean more = true;
+    while (more == true) {
+      readSpreadsheet();
+      narrowRecipes();
+      printRecipes();
+      measurments();
+      more();
+    }
+
+}
