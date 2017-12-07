5ecfbcb0b5c0badc001ccb3cb27031a94c9480b2
lbernstein98 <lbernstein@brandeis.edu>
2017-12-06 20:02:33 -0500

    ??????????????

--- FinalProject3.java
+++ FinalProject3.java
@@ -1,30 +1,30 @@
 public class FinalProject3 {
 
-  //declare static arrays
-  static int dbSize = 54;
-  static String[] name = new String[dbSize];
-  static String[] typeList = new String[dbSize];
-  static String[] description = new String[dbSize];
-  static double[] timeList = new double[dbSize];
-  static double[] calories = new double[dbSize];
-  static int[] servingsList = new int[dbSize];
-  static double[] meas1 = new double[dbSize];
-  static String[] ing1 = new String[dbSize];
-  static double[] meas2 = new double[dbSize];
-  static String[] ing2 = new String[dbSize];
-  static double[] meas3 = new double[dbSize];
-  static String[] ing3 = new String[dbSize];
-  static double[] meas4 = new double[dbSize];
-  static String[] ing4 = new String[dbSize];
-  static double[] meas5 = new double[dbSize];
-  static String[] ing5 = new String[dbSize];
-  static double[] meas6 = new double[dbSize];
-  static String[] ing6 = new String[dbSize];
-  static double[] meas7 = new double[dbSize];
-  static String[] ing7 = new String[dbSize];
-  static double[] meas8 = new double[dbSize];
-  static String[] ing8 = new String[dbSize];
-  static String[] directions = new String[dbSize];
+  //declare public static arrays
+  public static int dbSize = 54;
+  public static String[] name = new String[dbSize];
+  public static String[] typeList = new String[dbSize];
+  public static String[] description = new String[dbSize];
+  public static double[] timeList = new double[dbSize];
+  public static double[] calories = new double[dbSize];
+  public static int[] servingsList = new int[dbSize];
+  public static double[] meas1 = new double[dbSize];
+  public static String[] ing1 = new String[dbSize];
+  public static double[] meas2 = new double[dbSize];
+  public static String[] ing2 = new String[dbSize];
+  public static double[] meas3 = new double[dbSize];
+  public static String[] ing3 = new String[dbSize];
+  public static double[] meas4 = new double[dbSize];
+  public static String[] ing4 = new String[dbSize];
+  public static double[] meas5 = new double[dbSize];
+  public static String[] ing5 = new String[dbSize];
+  public static double[] meas6 = new double[dbSize];
+  public static String[] ing6 = new String[dbSize];
+  public static double[] meas7 = new double[dbSize];
+  public static String[] ing7 = new String[dbSize];
+  public static double[] meas8 = new double[dbSize];
+  public static String[] ing8 = new String[dbSize];
+  public static String[] directions = new String[dbSize];
 
   /**
   The main method
@@ -34,7 +34,7 @@ public class FinalProject3 {
     boolean moreInput = true;                                   //initialize boolean variables
     boolean moreRecipes = true;
     while (moreInput == true) {
-      readSpreadsheet();                                        //read data from excel file
+      Spreadsheet.readSpreadsheet();                            //read data from excel file
       welcome();                                                //print welcome message
       int servings = narrowRecipes();                           //get user's criteria for recipe and print the title of recipes that fit in a list
       while (moreRecipes == true){                              //as long as the user wants to get recipes from the list
@@ -57,68 +57,6 @@ public class FinalProject3 {
   public static void welcome() {
     TextIO.putf("%nWelcome to the Recipe Generator! %n");
     TextIO.putf("The app will help you make the perfect recipes for your meal%n%n%n");
-  }
-
-
-  /**
-  read information from a CSV file
-  no parameters
-  no return
-  */
-  public static void readSpreadsheet(){
-    TextIO.readFile("Recipes.csv");                           //read from file
-    TextIO.getln();                                           //ignore the first row (title of the columns)
-    int pos=0;
-    while (!TextIO.eof()){                                    //while it is not at the end of the file
-      String line = TextIO.getln();                           //read in a line of data
-      String[] fields = line.split(",");                      //split the line at commas
-      readStrings(pos, fields);                               //set the values in the arrays
-      readNumbers(pos, fields);
-      pos++;                                                  //increment the array position
-    }
-  }
-
-
-  /**
-  taking strings from the CSV file and assigning them to arrays
-  @param pos the position in the array that a value will be assigned to
-  @param fields an array of strings that contains the values to be assigned
-  no return since these are static arrays
-  */
-  public static void readStrings(int pos, String[] fields){
-    name[pos] = fields[0];
-    typeList[pos] = fields[1];
-    description[pos] = fields[2];
-    ing1[pos] = fields[7];
-    ing2[pos] = fields[9];
-    ing3[pos] = fields[11];
-    ing4[pos] = fields[13];
-    ing5[pos] = fields[15];
-    ing6[pos] = fields[17];
-    ing7[pos] = fields[19];
-    ing8[pos] = fields[21];
-    directions[pos] = fields[22];
-  }
-
-
-  /**
-  taking strings from the CSV file, converting them to doubles or ints, and assigning them to arrays
-  @param pos the position in the array that a value will be assigned to
-  @param fields and array of strings that contains the value to be assigned
-  no return since these are static arrays
-  */
-  public static void readNumbers(int pos, String[] fields){
-    timeList[pos] = Double.parseDouble(fields[3]);
-    calories[pos] = Double.parseDouble(fields[4]);
-    servingsList[pos] = Integer.parseInt(fields[5]);
-    meas1[pos] = Double.parseDouble(fields[6]);
-    meas2[pos] = Double.parseDouble(fields[8]);
-    meas3[pos] = Double.parseDouble(fields[10]);
-    meas4[pos] = Double.parseDouble(fields[12]);
-    meas5[pos] = Double.parseDouble(fields[14]);
-    meas6[pos] = Double.parseDouble(fields[16]);
-    meas7[pos] = Double.parseDouble(fields[18]);
-    meas8[pos] = Double.parseDouble(fields[20]);
   }
 
 
